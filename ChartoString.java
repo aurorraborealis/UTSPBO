309f38c2df55fa8b41cb9c81d7a687c8e0b6662b
Asusagawa Aurrora <49149529+aurorraborealis@users.noreply.github.com>
2020-09-08 20:54:29 +0700

    Add files via upload

---
+++ ChartoString.java
@@ -0,0 +1,39 @@
+package StringUTS;
+
+public class ChartoString {
+
+  public static void main(String[] args) {
+    
+    String chara = "ABCDEFGHIJKLMNOPQRSTUVWXYZ#-";
+    String numIn = "5-0-20-25-0-13";
+    
+    //method split untuk memisahkan string supaya menjadi array
+    String[] ex = numIn.split("-");
+    int[] n = new int[ ex.length ];
+    
+    //Merubah tipe data hasil pemisahan string supaya bisa menjadi tipe data integer
+    for(int i = 0 ; i < n.length ; i++) {
+       n[i] = Integer.parseInt(ex[i]);
+    };
+    
+    //Mengambil character dari variabel chara ke dalam array kemudian di susun sesuai index pada variabel numIn
+    char[] word  = new char[ ex.length];
+    for(int u=0; u<n.length; u++){
+       word[u] = chara.charAt(n[u]);
+    };
+    
+    //Variabel word yang berupa array di output kan sesuai index
+    for(int o=0; o<word.length; o++){
+      System.out.print(word[o]);
+    };
+    System.out.println();
+    for(int o=0; o<word.length; o++){
+      System.out.print(word[o]+",");
+    };
+    System.out.println();
+    
+    //Sekian dan Terimakasih
+    
+  }
+  
+}
